73f2b145e7bd9e446461d7e34f3cc021f99465bf
Felipe Prince <luizfas@dcc.ufrj.br>
2019-11-01 06:55:29 -0300

    Questao 2 feita

--- Questao_2.c
+++ Questao_2.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
-//Funcao Limpa a tela.
-void clrscr(){
-    system("@cls||clear");
-}
+#include <string.h>
 
 typedef struct ENDERECO{
 	char rua[100], comple[30], bairro[50], cep[10];
@@ -28,48 +23,62 @@ typedef struct{
 	DATA data;
 }PESSOA;
 
-/*Prototipos de Funcao*/
-void menu(PESSOA agenda[], int *tam, int max);
-void InserirPessoa(PESSOA agenda[], int *tam, int max);
-void recebeDADOS(PESSOA agenda[], int x);
-void retiraPessoa(PESSOA agenda[], int *tam);
-void imprimeDados(PESSOA agenda[], int numPessoa);
-void mostraDados(PESSOA pessoa);
-void buscaPorMes(PESSOA agenda[],int *tam);
-int encontraNome(PESSOA agenda[], int *x);
-void ordenaDados(PESSOA agenda[], int *tam);
 
+/*Prototipos de Funcao*/
+void clrscr();
+void menu(PESSOA agenda[], int max);
+void InserePessoa(PESSOA agenda[], int *tam, int max);
+void RecebeDADOS(PESSOA agenda[], int x);
+void RetiraPessoa(PESSOA agenda[], int *tam);
+void EncontraNome(PESSOA agenda[], int x);
+void BuscaPorMes(PESSOA agenda[],int tam);
+void ImprimeDados(PESSOA agenda[], int numPessoa);
+void MostraDados(PESSOA pessoa);
+void OrdenaDados(PESSOA agenda[], int tam);
 
 
 
 int main(){
-	int npessoas, tam=0;
+	int npessoas;
 	
-	while (1){
-		printf("Digite a quantidade de pessoas:");
+	clrscr();
+	while(1){
+		printf("Digite a quantidade de pessoas: ");
 		scanf("%d", &npessoas);
-		if (npessoas > 0 && npessoas <= 1000){
+		
+		if(npessoas > 0 && npessoas <= 100)
 			break;
-		}
+
+		printf("A quantidade deve ser de 1 a 100.\n\n");
 	}
 
 	PESSOA agenda[npessoas];
 
 	clrscr();
-    menu(agenda, &tam, npessoas);
-    
+    menu(agenda, npessoas);
+
+    printf("O sistema se encerrou.\n");
+    printf("Agradecemos a preferência! :)\n\n");
 	return 0;
 }
 
-void menu(PESSOA agenda[], int *tam, int max){
+
+//Funcao Limpa a tela.
+void clrscr(){
+    system("@cls||clear");
+}
+
+
+void menu(PESSOA agenda[], int max){
+	int tam = 0;
 	char opcao;
 
 	do{
-		printf("\n1 - Inserir pessoa\n");	
-		printf("2 - Deletar pessoa\n");
-		printf("3 - Imprimir agenda\n");
+		printf("\n1 - Inserir Pessoa\n");	
+		printf("2 - Deletar Pessoa\n");
+		printf("3 - Buscar Pessoa\n");
 		printf("4 - Buscar Aniversariantes\n");
-		printf("5 - Buscar Pessoa\n");
+		printf("5 - Imprimir Agenda\n");
 		printf("6 - Sair\n\n");
 
 		while(1){
@@ -79,44 +88,41 @@ void menu(PESSOA agenda[], int *tam, int max){
 			if(opcao>='1' && opcao<='6')
 				break;
 			
-			clrscr();
 			printf("A opção não é válida!\n\n");	
 		}
 
+		clrscr();
 		switch(opcao){
 			case '1':
-				clrscr();
-				InserirPessoa(agenda, tam, max);
+				InserePessoa(agenda, &tam, max);
 				break;
 
 			case '2':
-				clrscr();
-				retiraPessoa(agenda, tam);
+				RetiraPessoa(agenda, &tam);
 				break;
 
 			case '3':
-				clrscr();
-				imprimeDados(agenda, *tam);
-				break;
-			case '4':
-				clrscr();
-				buscaPorMes(agenda, tam);
+				EncontraNome(agenda, tam);
 				break;
 
+			case '4':
+				BuscaPorMes(agenda, tam);
+				break;
+			
 			case '5':
-				clrscr();
-				encontraNome(agenda, tam);
+				ImprimeDados(agenda, tam);
 				break;
 
 			default:
 				return;
 		}
+
 		printf("\n\n\n-------------------------------------------------------\n\n");
 	}while(1);
 }
 
 
-void InserirPessoa(PESSOA agenda[], int *tam, int max){
+void InserePessoa(PESSOA agenda[], int *tam, int max){
 	int quant, falta = max-*tam;
 
 	while(1){
@@ -125,67 +131,86 @@ void InserirPessoa(PESSOA agenda[], int *tam, int max){
 		
 		if(quant>0 && quant<=falta)
 			break;
-
+		
+		if(falta==1){
+			printf("Só mais uma pessoa pode ser adicionada.\n\n");	
+			continue;
+		}
 		printf("A quantidade deve ser de 1 a %d.\n\n", falta);
 	}
 
-	recebeDADOS(agenda, quant);
+	clrscr();
+	RecebeDADOS(agenda, quant);
 	*tam += quant;
-	ordenaDados(agenda, tam);
+	OrdenaDados(agenda, *tam);
 }
 
 
-
-void recebeDADOS(PESSOA agenda[], int quant){
+void RecebeDADOS(PESSOA agenda[], int quant){
 	int i;
-	for (i = 0; i < quant; i++){
+	for(i=0; i<quant; i++){
 		printf("\nCONTATO %d\n", i+1);
-		printf("Nome:");
+		printf("Nome: ");
 		scanf(" %[^\n]s", agenda[i].nome);
-		printf("E-mail:");
+		
+		printf("E-mail: ");
 		scanf(" %[^\n]s", agenda[i].email);
-		printf("Data de aniversario(DD MM AA):");
-		//Por algum motivo estava aparecendo erros se eu coloca-se sem &.
+		
+		printf("Data de aniversário(DD MM AA): ");
 		scanf("%d %d %d", &(agenda[i].data.dia), &(agenda[i].data.mes), &(agenda[i].data.ano));
-		printf("*Telefone*\n");
-		printf("DD:");
+		
+		printf("\n*Telefone*\n");
+		printf("DD: ");
 		scanf(" %[^\n]s", agenda[i].tel.ddd);
-		printf("Numero:");
+		
+		printf("Número: ");
 		scanf(" %[^\n]s", agenda[i].tel.numero);
-		printf("\n*******ENDERECO*******\n");
-		printf("Rua:");
+		
+		printf("\n*******ENDEREÇO*******\n");
+		printf("Rua: ");
 		scanf(" %[^\n]s", agenda[i].endereco.rua);
-		printf("Numero:");
+		
+		printf("Número: ");
 		scanf("%d", &(agenda[i].endereco.numero));
-		printf("Complemento:");
+		
+		printf("Complemento: ");
 		scanf(" %[^\n]s", agenda[i].endereco.comple);
-		printf("Bairro:");
+		
+		printf("Bairro: ");
 		scanf(" %[^\n]s", agenda[i].endereco.bairro);
-		printf("CEP:");
+		
+		printf("CEP: ");
 		scanf(" %[^\n]s", agenda[i].endereco.cep);
-		printf("Cidade:");
+		
+		printf("Cidade: ");
 		scanf(" %[^\n]s", agenda[i].endereco.cidade);
-		printf("Estado:");
+		
+		printf("Estado: ");
 		scanf(" %[^\n]s", agenda[i].endereco.estado);
-		printf("Pais:");
-		scanf(" %[^\n]s", agenda[i].endereco.pais);
-		printf("Observacoes:");
+		
+		printf("País: ");
+		scanf(" %[^\n]s ", agenda[i].endereco.pais);	
+
+		printf("Observações: ");
 		scanf(" %[^\n]s", agenda[i].obs);
+
+		if(i != quant-1)
+			printf("\n\n-------------------------------------------------------\n");
 	}
 }
 
 
-void retiraPessoa(PESSOA agenda[], int *tam){
+void RetiraPessoa(PESSOA agenda[], int *tam){
     
     int i, b = 0;
     char nomeRemover[255];
 
 	if(*tam==0){
-		printf("Não tem pessoas a serem excluídas!\n");
+		printf("Não há pessoas a serem excluídas!\n");
 		return; 
 	}
     
-    printf("Entre com nome da pessoa para remover da Agenda: ");
+    printf("Entre com nome da pessoa que deseja remover da Agenda: ");
     scanf(" %[^\n]s", nomeRemover);
     
     for(i = 0; i < *tam - 1; i++){
@@ -197,53 +222,46 @@ void retiraPessoa(PESSOA agenda[], int *tam){
 	PESSOA temp = {};
     agenda[*tam - 1] = temp;
 	(*tam)--;
-	ordenaDados(agenda, tam);
+	OrdenaDados(agenda, *tam);
 }
 
 
-void imprimeDados(PESSOA agenda[], int quant){
-    int i = 0;
-	if (quant != 0){
-		for(i=0; i<quant; i++){
-			printf("\nCONTATO %d\n\n", i+1);
-			mostraDados(agenda[i]);
-		}
-	}else{
-		printf("Digite algum contato, para exibir.\n");
+void EncontraNome(PESSOA agenda[], int tam){
+	int i, quant;
+	char nome[255];
+	
+	if(tam==0){
+		printf("Por favor, preencha a agenda antes de buscar por uma pessoa.\n");
+		return;
 	}
+
+	printf("Digite o primeiro nome da pessoa que deseja encontrar: ");
+	scanf(" %s", nome);
+	quant = strlen(nome);
+	
+	for(i=0; i<tam; i++){
+		if(strncmp(nome, agenda[i].nome, quant)==0){
+			printf("\n");
+			MostraDados(agenda[i]);
+			return;
+		}
+	}
+
+	printf("O nome digitado não existe na agenda!\n");
 }
 
-void mostraDados(PESSOA pessoa){
-	printf("Nome: %s\n", pessoa.nome);
-    printf("Data de aniversario: %d/%d/%d.\n", pessoa.data.dia, pessoa.data.mes, pessoa.data.ano);
-    printf("Telefone: %s %s\n", pessoa.tel.ddd, pessoa.tel.numero);
-    printf("E-mail: %s\n", pessoa.email);
-    
-    printf("\nENDERECO: \n\n");
-    
-    printf("Rua: %s\n", pessoa.endereco.rua);
-    printf("Numero: %d\n", pessoa.endereco.numero);
-    printf("Complemento: %s\n", pessoa.endereco.comple);
-    printf("Bairro: %s\n", pessoa.endereco.bairro);
-    printf("Cidade: %s\n", pessoa.endereco.cidade);
-    printf("Estado: %s\n", pessoa.endereco.estado);
-    printf("Pais: %s\n\n", pessoa.endereco.pais);
-    printf("CEP: %s\n", pessoa.endereco.cep);
-    
-    printf("Observacoes: %s\n\n", pessoa.obs);
-}
 
-void buscaPorMes(PESSOA agenda[], int *tam){
+void BuscaPorMes(PESSOA agenda[], int tam){
 	//Esta Funcao ira encontrar todas as pessoas que fazem aniversario no mesmo mes e dia.
-	if (*tam != 0){
+	if (tam != 0){
 		int i, mesAniver, diaAniver;
-		printf("Digite o dia que deseja analisar(dois digitos):");
+		printf("Digite o dia que deseja analisar(dois dígitos): ");
 		scanf("%d", &diaAniver);
-		printf("Digite os dados do mes que deseja analisar(dois digitos):");
+		printf("Digite os dados do mês que deseja analisar(dois dígitos): ");
 		scanf("%d", &mesAniver);
-		for(i = 0; i < *tam; i++){
+		for(i = 0; i < tam; i++){
 			if(agenda[i].data.mes == mesAniver && agenda[i].data.dia == diaAniver){
-				imprimeDados(agenda, i);
+				ImprimeDados(agenda, i);
 			}
 		}
 	}
@@ -253,47 +271,48 @@ void buscaPorMes(PESSOA agenda[], int *tam){
 	}
 }
 
-int encontraNome(PESSOA agenda[], int *x){
-	//Vou utilizar vetor pois quero percorrer a palavra.
-	char nome[255];
-	int i, j, aux;
-	if (*x != 0){
-		printf("Digite o primeiro da pessoa que deseja encontrar:");
-		scanf(" %[^\n]s", nome);
-		for (i = 0; i < *x; i++){
-			j = 0;
-			aux = 0;
-			// Um laco que vai comparando letra a letra, e nao pega o espaco, ele acaba em funcao do primeiro nome.
-			while (agenda[i].nome[j] == nome[j] && agenda[i].nome[j] != ' '){
-				aux++;//Conta para ver quantas letras tem em comum, fazendo letra a letra
-				if (j <= strlen(nome)){
-					if (aux == strlen(nome)){
-						//Vou colocar a funcao do felipe aqui.
-						imprimeDados(agenda, i);
-						return 0;
-					}
-				}else{
-					break;
-				}
-				j++;
-			}
+
+void ImprimeDados(PESSOA agenda[], int quant){
+    int i = 0;
+	if(quant != 0){
+		for(i=0; i<quant; i++){
+			printf("\nCONTATO %d\n\n", i+1);
+			MostraDados(agenda[i]);
 		}
-		printf("O nome digitado nao existe na agenda!\n");
-		return 0;
 	}else{
-		printf("Por favor, preencha a agenda antes de buscar por uma pessoa.\n");
-		return 0;
+		printf("Digite algum contato, para exibir.\n");
 	}
-	
 }
 
-void ordenaDados(PESSOA agenda[], int *tam){
-	int i, j;
-	PESSOA aux[*tam];
 
-	for (i = 1; i < *tam; i++){
-		for (j = 0; j < *tam - 1; j++){
-			if (strcmp(agenda[j].nome, agenda[j+1].nome) > 0){
+void MostraDados(PESSOA pessoa){
+	printf("Nome: %s\n", pessoa.nome);
+    printf("Data de aniversário: %d/%d/%d.\n", pessoa.data.dia, pessoa.data.mes, pessoa.data.ano);
+    printf("Telefone: %s %s\n", pessoa.tel.ddd, pessoa.tel.numero);
+    printf("E-mail: %s\n", pessoa.email);
+    
+    printf("\nENDEREÇO: \n\n");
+    
+    printf("Rua: %s\n", pessoa.endereco.rua);
+    printf("Número: %d\n", pessoa.endereco.numero);
+    printf("Complemento: %s\n", pessoa.endereco.comple);
+    printf("Bairro: %s\n", pessoa.endereco.bairro);
+    printf("Cidade: %s\n", pessoa.endereco.cidade);
+    printf("Estado: %s\n", pessoa.endereco.estado);
+    printf("País: %s\n\n", pessoa.endereco.pais);
+    printf("CEP: %s\n", pessoa.endereco.cep);
+    
+    printf("Observações: %s\n\n", pessoa.obs);
+}
+
+
+void OrdenaDados(PESSOA agenda[], int tam){
+	int i, j;
+	PESSOA aux[tam];
+
+	for(i = 1; i < tam; i++){
+		for(j = 0; j < tam - 1; j++){
+			if(strcmp(agenda[j].nome, agenda[j+1].nome) > 0){
 				aux[0] = agenda[j];
 				agenda[j] = agenda[j+1];
 				agenda[j+1] = aux[0];
